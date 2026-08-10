adcdc3c3868300e598c5f32c925d4fd19e63d3f8
Aditya Mishra <adityamishra19880@gmail.com>
2026-08-11 00:31:22 +0530

    a simple project of context api

---
+++ context-api/src/Components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
+
+export default function Profile(){
+    const {user} = useContext(UserContext);
+
+    if(!user) return <div>please login</div> 
+
+    return <div>Welcome, {user.username}</div>
+}
