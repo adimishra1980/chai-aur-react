5ff7a7ad27c31d650aa67f925b854c64309d5f3d
Aditya Mishra <adityamishra19880@gmail.com>
2026-08-12 13:40:04 +0530

    megaBlog: add components

--- megaBlog/src/components/container/Container.jsx
+++ megaBlog/src/components/container/Container.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import Logo from "../index"
+import Logo from "../Logo";
+
 
 function Footer() {
   return (
--- megaBlog/src/components/index.js
+++ megaBlog/src/components/index.js
@@ -1,4 +1,8 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import LogoutBtn from "./Header/LogoutBtn"
+import Logo from "./Logo"
+import Container from "./container/Container"
+import Input from "./Input";
 
-export { Header, Footer };
+export { Header, Footer, LogoutBtn, Logo, Container, Input };
