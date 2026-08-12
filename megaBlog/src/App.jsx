68334bb1de4e1b5c33c670ef88d5139ba5c9e750
Aditya Mishra <adityamishra19880@gmail.com>
2026-08-12 10:16:01 +0530

    store setup and tailwind config

--- megaBlog/src/App.jsx
+++ megaBlog/src/App.jsx
@@ -1,5 +1,49 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.service";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components/index";
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <></>;
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching current user:", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+        <div className="w-full block">
+          <Header />
+          <main>
+            {/* <Outlet /> */}
+          </main>
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
