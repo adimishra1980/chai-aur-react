4952f1b2ebd9432fae850664a2828067fb5bc6b1
Aditya Mishra <adityamishra19880@gmail.com>
2026-08-12 20:24:08 +0530

    megaBlog: add pages for AddPost, AllPosts, EditPost, Home, Login, Post, and Signup components

---
+++ megaBlog/src/components/pages/AllPosts.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { Container, PostCard } from "../index";
+import databaseService from "../../appwrite/database.service";
+
+function AllPosts() {
+
+    const [posts, setPosts] = useState([])
+    
+    useEffect(() => {
+        databaseService.getPosts([])
+            .then((posts) => {
+                if(posts){
+                    setPosts(posts.documents)
+                }
+            })
+            .catch((err) => console.error(err))
+    }, [])
+
+  return (
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default AllPosts;
